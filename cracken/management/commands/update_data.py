ab15921bc8e9fd8c82aa9eb3693148834996e3b1
OrerezTAU <100091249+OrerezTAU@users.noreply.github.com>
2023-05-19 01:37:04 +0300

    Created the update_data.py file to create a management command. Started implementing the logic within the command using Reddit API and pandas

---
+++ cracken/management/commands/update_data.py
@@ -0,0 +1,53 @@
+import praw
+import pandas as pd
+
+
+reddit = praw.Reddit(
+    client_id="dk3990BeRf2SHUD8omrOJQ",
+    client_secret="Qf0oesji7FZOtSDxIYw8hErqogBH5Q",
+    user_agent="crackwatch scraper bot 1.0 by /u/Th3-3rr0r",
+)
+
+subreddit = reddit.subreddit("CrackWatch") # subreddit to scrape
+
+search_results = subreddit.search('Daily Releases', time_filter='day', sort='new') # search for the daily releases thread
+
+submission = next(search_results) # get the first result (today's thread)
+
+def extract_table_contents(table_text):
+    """Extracts the column names and data rows from a table-like string."""
+    rows = table_text.strip().split('\n')
+    column_names = [col.strip() for col in rows[0].split('|') if col.strip()]
+    data_rows = []
+
+    for row in rows[2:]: # skip the column names row and the text directions row
+        cells = [cell.strip() for cell in row.split('|') if cell.strip()]
+        if len(cells) == len(column_names):
+            data_rows.append(cells)
+
+    return column_names, data_rows
+
+
+def extract_table_from_thread():
+    """Extracts the table from the submission's (thread) selftext."""
+    if '|' in submission.selftext:
+        # Extract the table section from selftext
+        start_index = submission.selftext.find('|')
+        end_index = submission.selftext.find('&nbsp;', start_index) # the table ends with '&nbsp;' (non-breaking space)
+        table_text = submission.selftext[start_index:end_index]
+
+        column_names, data_rows = extract_table_contents(table_text)
+
+        if column_names and data_rows:
+            df = pd.DataFrame(data_rows, columns=column_names) # create a pandas dataframe from the table data
+            return df
+        else:
+            print("No table found in the submission.")
+    else:
+        print("No table-like structure found in the submission.")
+    print("---")
+
+
+# Check if the submission has a table-like structure
+DataFrame = extract_table_from_thread()
+print(DataFrame)
