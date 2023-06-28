3270d083b6f9c447efa075a18f0e3c0994503cb5
OrerezTAU <100091249+OrerezTAU@users.noreply.github.com>
2023-06-28 21:57:15 +0300

    Rest API works and connects frontend to DB

--- WarezArena/settings.py
+++ WarezArena/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
-from celery.schedules import crontab
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "cracken.apps.CrackenConfig"
+    "cracken.apps.CrackenConfig",
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +57,7 @@ ROOT_URLCONF = "WarezArena.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'frontend', 'build')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -101,6 +101,12 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
+REST_FRAMEWORK = {
+     'DEFAULT_RENDERER_CLASSES': (
+         'rest_framework.renderers.JSONRenderer',
+     )
+ }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
@@ -118,6 +124,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- cracken/urls.py
+++ cracken/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from cracken.views import index, game, warez_group, store, game_detail, warez_group_detail, store_detail
 
 urlpatterns = [
-    #path('', views.index, name='index'),
-
+    path('', index, name='index'),
+    path("games/", game, name="games"),
+    path("games/<int:pk>/", game_detail, name="game_detail"),
+    path("warez_groups/", warez_group, name="warez_groups"),
+    path("warez_groups/<int:pk>/", warez_group_detail, name="warez_group_detail"),
+    path("stores/", store, name="stores"),
+    path("stores/<int:pk>/", store_detail, name="store_detail"),
 ]
 
