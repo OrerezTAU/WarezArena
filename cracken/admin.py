ef2fc6318c5d157d93a80374672d0dea549a2955
orerez <orerez@mail.tau.ac.il>
2023-08-01 22:38:05 +0300

    Registered Game,Store and WarezGroup models in the admin.py file

--- cracken/admin.py
+++ cracken/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game, WarezGroup, Store
+
+
+class GameInline(admin.TabularInline):
+    model = Game
+    extra = 0
+
+
+# Register the Admin classes for Game using the decorator
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('name', 'score', 'num_reviews', 'nfo_link', 'crack_date')
+    list_filter = ('crack_date', 'num_reviews')
+    fields = ['name', ('score', 'num_reviews'), 'nfo_link', 'crack_date', 'warez_group']
+
+
+# Register the Admin classes for Store using the decorator
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url', 'description')
+    fields = ['name', 'url', 'description']
+
+
+# Register the Admin classes for WarezGroup using the decorator
+@admin.register(WarezGroup)
+class WarezGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'year_founded')
+    fields = ('name', 'description', 'year_founded')
+    inlines = [GameInline]
